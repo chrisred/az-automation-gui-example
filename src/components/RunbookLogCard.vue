ee08720b4412e01da9d7b787deac5359a6bac704
Chris Redit <contact@redit.name>
2024-03-30 15:56:09 +0000

    Add automatic scroll to the log card
    Scroll the log output text to show the newest entries.

--- src/components/RunbookLogCard.vue
+++ src/components/RunbookLogCard.vue
@@ -1,25 +1,55 @@
 <script setup>
+  import { ref, onMounted, onBeforeUnmount } from 'vue'
+  import { useGoTo } from 'vuetify'
+
   const props = defineProps({ messages: { type: Array, required: true }})
+  const goTo = useGoTo()
+  const resizeEvent = new ResizeObserver(onResize)
+  const logContainer = ref(null)
+  const logCard = ref(null)
+
   defineEmits(['clear'])
+
+  onMounted(() => {
+    resizeEvent.observe(logCard.value.$el)
+  })
+
+  onBeforeUnmount(() => {
+    resizeEvent.unobserve(logCard.value.$el)
+  })
+
+  function onResize()
+  {
+    // fix the warning "Vuetify: Scroll target is not reachable" when only the logCard offsetHeight is used
+    const targetOffset = logCard.value.$el.offsetHeight - logContainer.value.$el.offsetHeight
+
+    if (targetOffset > 0)
+    {
+      // automatically scroll the log card to show new output
+      goTo(targetOffset, {container: '#logging-container', duration:0, offset:32})
+    }
+  }
 </script>
 
 <template>
   <v-card class="logging mt-4" title="Runbook Logging" variant="tonal" max-height="350" elevation="1">
     <v-divider />
-    <v-card-text class="logging">
-      <ul class="logging-text">
-        <!-- eslint-disable-next-line vue/require-v-for-key -->
-        <li v-for="message in props.messages">
-          <span v-if="message.type == 'error'" class="text-red font-weight-bold">
-            {{ message.text }}
-          </span>
-          <span v-else-if="message.type == 'warn'" class="text-yellow font-weight-bold">
-            {{ message.text }}
-          </span>
-          <span v-else>{{ message.text }}</span>
-        </li>
-      </ul>
-    </v-card-text>
+    <v-container id="logging-container" ref="logContainer" class="logging-overflow">
+      <v-card-text ref="logCard">
+        <ul class="logging-text">
+          <!-- eslint-disable-next-line vue/require-v-for-key -->
+          <li v-for="message in props.messages">
+            <span v-if="message.type == 'error'" class="text-red font-weight-bold">
+              {{ message.text }}
+            </span>
+            <span v-else-if="message.type == 'warn'" class="text-yellow font-weight-bold">
+              {{ message.text }}
+            </span>
+            <span v-else>{{ message.text }}</span>
+          </li>
+        </ul>
+      </v-card-text>
+    </v-container>
     <v-card-actions>
       <v-spacer />
       <v-btn variant="text" @click="$emit('clear')">Clear</v-btn>
@@ -33,7 +63,7 @@
     flex-direction: column;
   }
 
-  .v-card-text.logging {
+  .v-container.logging-overflow {
     overflow: auto;
   }
 
